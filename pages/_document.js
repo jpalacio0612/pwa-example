0abe11c1b32a3e8ae998b2d33032fbb9330fde3e
jpalacio0612 <jpalacio0612@gmail.com>
2022-04-28 13:08:59 -0500

    modify images

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
           <meta name="theme-color" content="#fff" />
-          <link
+          {/* <link
             rel="apple-touch-startup-image"
             href="/apple_splash_2048.png"
             sizes="2048x2732"
@@ -42,57 +42,57 @@ class MyDocument extends Document {
             rel="apple-touch-startup-image"
             href="/apple_splash_640.png"
             sizes="640x1136"
-          />
-          {/* <link
-            href="splashscreens/iphone5_splash.png"
+          /> */}
+          <link
+            href="/iphone5_splash.png"
             media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="splashscreens/iphone6_splash.png"
+            href="/iphone6_splash.png"
             media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="splashscreens/iphoneplus_splash.png"
+            href="/iphoneplus_splash.png"
             media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="splashscreens/iphonex_splash.png"
+            href="/iphonex_splash.png"
             media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="splashscreens/iphonexr_splash.png"
+            href="/iphonexr_splash.png"
             media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="splashscreens/iphonexsmax_splash.png"
+            href="/iphonexsmax_splash.png"
             media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="splashscreens/ipad_splash.png"
+            href="/ipad_splash.png"
             media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="splashscreens/ipadpro1_splash.png"
+            href="/ipadpro1_splash.png"
             media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="splashscreens/ipadpro3_splash.png"
+            href="/ipadpro3_splash.png"
             media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="splashscreens/ipadpro2_splash.png"
+            href="/ipadpro2_splash.png"
             media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
             rel="apple-touch-startup-image"
-          /> */}
+          />
         </Head>
         <body>
           <Main />
