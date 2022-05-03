3112d754d75ede83524d8a956fbef9827ded954e
jpalacio0612 <jpalacio0612@gmail.com>
2022-05-03 08:04:22 -0500

    add install button

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import { useEffect, useState } from "react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [state, setState] = useState(null);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      setState(e);
+    });
+  }, []);
+
+  function handleInstall() {
+    if (state) {
+      state.prompt();
+      state.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("Installed");
+        } else {
+          console.log("No Installed");
+        }
+      });
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={handleInstall}>Install</button>
+      <Component {...pageProps} />;
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
